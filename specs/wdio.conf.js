e9e0aeafce78a541513f1f2487184720651169b5
Robert Bradley <rbradaws@amazon.co.uk>
2023-07-04 14:48:18 +0100

    testing

--- specs/wdio.conf.js
+++ specs/wdio.conf.js
@@ -45,7 +45,7 @@ exports.config = {
             console.log(specs);
             // Emit the session ID event
             console.log('Session ID:', sessionId);
-            const outputFile = __dirname + `sessions.log`;
+            const outputFile = `./sessions.log`;
             fs.appendFile(outputFile, `${specFilename}:${sessionId}\n`, function (err) {
                 if (err) throw err;
                 console.log('Session info Saved to sessions.log');
