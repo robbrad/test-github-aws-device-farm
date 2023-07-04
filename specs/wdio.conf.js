2c05bd135eaf264178f6d4d67a61d1fc25ebcdd4
Robert Bradley <rbradaws@amazon.co.uk>
2023-07-04 12:07:14 +0100

    testing

---
+++ specs/wdio.conf.js
@@ -0,0 +1,74 @@
+"use strict";
+const path = require('path');
+const { launcher } = require('../dist/wdio-aws-device-farm-service')
+
+const fs = require('fs');
+
+exports.config = {
+    runner: "local",
+    specs: [path.resolve(__dirname, '*.test.js')],
+    maxInstances: 12,
+    capabilities: [
+        {
+            browserName: 'chrome',
+            acceptInsecureCerts: true,
+        },
+        {
+            browserName: 'firefox',
+            acceptInsecureCerts: true,
+        }
+    ],
+    logLevel: 'trace',
+    framework: 'mocha',
+    outputDir: __dirname,
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 30000,
+    },
+    services: [
+        [
+            launcher,
+            {
+                projectArn: process.env.PROJECT_ARN,
+            },
+        ],
+    ],
+    before: function (capabilities, specs, browser) {
+        if (browser.sessionId) {
+            // Access the session ID
+            const sessionId = browser.sessionId;
+            const specPath = specs[0];
+
+            const extension = path.extname(specPath);
+            const specFilename = path.basename(specPath,extension);
+            console.log(specs);
+            // Emit the session ID event
+            console.log('Session ID:', sessionId);
+            const outputFile = `sessions.log`;
+            fs.appendFile(outputFile, `${specFilename}:${sessionId}\n`, function (err) {
+                if (err) throw err;
+                console.log('Session info Saved to sessions.log');
+            });
+            
+        }
+
+    },
+
+};
+
+function checkForFile(fileName,callback)
+{
+    fs.exists(fileName, function (exists) {
+        if(exists)
+        {
+            callback();
+        }else
+        {
+            fs.writeFile(fileName, {flag: 'wx'}, function (err, data) 
+            { 
+                callback();
+            })
+        }
+    });
+}
